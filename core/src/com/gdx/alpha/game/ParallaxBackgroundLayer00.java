6e8687fb9f8bec97eedf749f9fa20fe8a251caec
roden1978 <rdn.homebox@gmail.com>
2019-06-24 23:20:05 +0500

    доработка параллельного фона и надписей на промежуточных экранах

--- core/src/com/gdx/alpha/game/ParallaxBackgroundLayer00.java
+++ core/src/com/gdx/alpha/game/ParallaxBackgroundLayer00.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Array;
 
 /**
- * Created by Admin on 18.02.15.
+ * Created by Ro|)e|\| on 18.02.15.
  */
 public class ParallaxBackgroundLayer00 extends Actor {
     private Array<TextureRegion> backgroundDownLayer0;
@@ -24,7 +24,7 @@ public class ParallaxBackgroundLayer00 extends Actor {
     private Vector2 upLayer02Position;
 
 
-    public ParallaxBackgroundLayer00(TextureAtlas backgroundAtlas){
+    ParallaxBackgroundLayer00(TextureAtlas backgroundAtlas){
         backgroundDownLayer0 = new Array<TextureRegion>(3);
         backgroundUpLayer0 = new Array<TextureRegion>(3);
 
@@ -33,11 +33,14 @@ public class ParallaxBackgroundLayer00 extends Actor {
             backgroundUpLayer0.add(backgroundAtlas.findRegion("layer01up"));
         }
 
-        downLayer00Position = new Vector2(Gdx.graphics.getWidth() - backgroundDownLayer0.get(0).getRegionWidth(), 0);
+        //downLayer00Position = new Vector2(Gdx.graphics.getWidth() - backgroundDownLayer0.get(0).getRegionWidth(), 0);
+        downLayer00Position = new Vector2(backgroundDownLayer0.get(0).getRegionWidth(), 0);
         downLayer01Position = new Vector2(0, 0);
         downLayer02Position = new Vector2(- backgroundDownLayer0.get(0).getRegionWidth(), 0);
 
-        upLayer00Position = new Vector2(Gdx.graphics.getWidth() - backgroundUpLayer0.get(0).getRegionWidth(),
+       /* upLayer00Position = new Vector2(Gdx.graphics.getWidth() - backgroundUpLayer0.get(0).getRegionWidth(),
+                Gdx.graphics.getHeight() - backgroundUpLayer0.get(0).getRegionHeight());*/
+        upLayer00Position = new Vector2(backgroundUpLayer0.get(0).getRegionWidth(),
                 Gdx.graphics.getHeight() - backgroundUpLayer0.get(0).getRegionHeight());
         upLayer01Position = new Vector2(0,Gdx.graphics.getHeight() - backgroundUpLayer0.get(0).getRegionHeight());
         upLayer02Position = new Vector2(- backgroundUpLayer0.get(0).getRegionWidth(),
@@ -52,16 +55,16 @@ public class ParallaxBackgroundLayer00 extends Actor {
         for (int i = 0; i < 3; i++) {
             switch (i){
                 case 0:
-                    batch.draw(backgroundDownLayer0.get(0),downLayer00Position.x, downLayer00Position.y);
-                    batch.draw(backgroundUpLayer0.get(0),upLayer00Position.x, upLayer00Position.y);
+                    batch.draw(backgroundDownLayer0.get(i),downLayer00Position.x, downLayer00Position.y);
+                    batch.draw(backgroundUpLayer0.get(i),upLayer00Position.x, upLayer00Position.y);
                     break;
                 case 1:
-                    batch.draw(backgroundUpLayer0.get(1),upLayer01Position.x, upLayer01Position.y);
-                    batch.draw(backgroundDownLayer0.get(1),downLayer01Position.x, downLayer01Position.y);
+                    batch.draw(backgroundUpLayer0.get(i),upLayer01Position.x, upLayer01Position.y);
+                    batch.draw(backgroundDownLayer0.get(i),downLayer01Position.x, downLayer01Position.y);
                     break;
                 case 2:
-                    batch.draw(backgroundUpLayer0.get(2),upLayer02Position.x, upLayer02Position.y);
-                    batch.draw(backgroundDownLayer0.get(2),downLayer02Position.x, downLayer02Position.y);
+                    batch.draw(backgroundUpLayer0.get(i),upLayer02Position.x, upLayer02Position.y);
+                    batch.draw(backgroundDownLayer0.get(i),downLayer02Position.x, downLayer02Position.y);
                     break;
             }
         }
@@ -70,8 +73,32 @@ public class ParallaxBackgroundLayer00 extends Actor {
     @Override
     public void act(float delta) {
         super.act(delta);
-
         downLayer00Position.x += 3;
+        downLayer01Position.x += 3;
+        downLayer02Position.x += 3;
+        if (downLayer00Position.x >= Gdx.graphics.getWidth())
+            downLayer00Position.x = - (backgroundDownLayer0.get(0).getRegionWidth() *2.0f -
+                    Gdx.graphics.getWidth()) - backgroundDownLayer0.get(0).getRegionWidth() + (downLayer00Position.x - Gdx.graphics.getWidth());
+        if (downLayer01Position.x >= Gdx.graphics.getWidth())
+            downLayer01Position.x = - (backgroundDownLayer0.get(0).getRegionWidth() *2.0f -
+                    Gdx.graphics.getWidth()) - backgroundDownLayer0.get(0).getRegionWidth() + (downLayer01Position.x - Gdx.graphics.getWidth());
+        if (downLayer02Position.x >= Gdx.graphics.getWidth())
+            downLayer02Position.x = - (backgroundDownLayer0.get(0).getRegionWidth() *2.0f -
+                    Gdx.graphics.getWidth()) - backgroundDownLayer0.get(0).getRegionWidth() + (downLayer02Position.x - Gdx.graphics.getWidth());
+
+        upLayer00Position.x += 3;
+        upLayer01Position.x += 3;
+        upLayer02Position.x += 3;
+        if (upLayer00Position.x >= Gdx.graphics.getWidth())
+            upLayer00Position.x = - (backgroundUpLayer0.get(0).getRegionWidth() *2.0f -
+                    Gdx.graphics.getWidth()) - backgroundUpLayer0.get(0).getRegionWidth() + (upLayer00Position.x - Gdx.graphics.getWidth());
+        if (upLayer01Position.x >= Gdx.graphics.getWidth())
+            upLayer01Position.x = - (backgroundUpLayer0.get(0).getRegionWidth() *2.0f -
+                    Gdx.graphics.getWidth()) - backgroundUpLayer0.get(0).getRegionWidth() + (upLayer01Position.x - Gdx.graphics.getWidth());
+        if (upLayer02Position.x >= Gdx.graphics.getWidth())
+            upLayer02Position.x = - (backgroundUpLayer0.get(0).getRegionWidth() *2.0f -
+                    Gdx.graphics.getWidth()) - backgroundUpLayer0.get(0).getRegionWidth() + (upLayer02Position.x - Gdx.graphics.getWidth());
+      /*  downLayer00Position.x += 3;
         downLayer01Position.x += 3;
         downLayer02Position.x += 3;
         if (downLayer00Position.x >= Gdx.graphics.getWidth())
@@ -89,7 +116,7 @@ public class ParallaxBackgroundLayer00 extends Actor {
         if (upLayer01Position.x >= Gdx.graphics.getWidth())
             upLayer01Position.x = -backgroundUpLayer0.get(0).getRegionWidth();
         if (upLayer02Position.x >= Gdx.graphics.getWidth())
-            upLayer02Position.x = -backgroundUpLayer0.get(0).getRegionWidth();
+            upLayer02Position.x = -backgroundUpLayer0.get(0).getRegionWidth();*/
 
     }
 }
